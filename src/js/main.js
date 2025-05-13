c485acef197812a6780f672a44b5ee2f2875b9f8
Kellen James <8484636@ups.com>
2025-05-13 15:15:44 -0400

    docs: add final JSDoc annotations to model and main entrypoint

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
+/**
+ * Application entry point.
+ * Initializes event listeners and logic via controller.
+ */
 import './controller.js';
--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,7 @@
+/**
+ * Global UI state for modal visibility.
+ * @type {{ isModalOpen: boolean }}
+ */
 export const state = {
   isModalOpen: false,
 };
